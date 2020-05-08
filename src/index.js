0faa499c2ab2729960711d655769877faa87ca17
Ben Regenspan <bregenspan@gmail.com>
2020-05-08 17:03:57 -0500

    Version without re-export

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import two from './two.js';
-import shared from './shared-re-exported.js';
+
+export function shared() {
+  console.log('shared');
+}
 
 console.log('"shared re-export" should be logged twice, once above this log line, once below');
 shared();
-
-export { shared as shared };
