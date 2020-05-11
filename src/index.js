c4a9f00e1e2b5a9aefa84f3bf526d2f88429f305
Ben Regenspan <bregenspan@gmail.com>
2020-05-11 11:18:51 -0500

    Work around issue by adjusting order of imports

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import two from './two.js';
 import shared from './shared.js';
+import two from './two.js';
 
 console.log('"shared import" should be logged twice, once above this log line, once below');
 shared();
